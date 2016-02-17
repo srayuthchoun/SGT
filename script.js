a942ee7d7a7def94cd8d65a83ebb9f29f5da7ce3
Nicholas Bidler <nomandoestheimpossible@gmail.com>
2016-02-17 13:52:37 -0800

    SGT v.5 - Nick - trying to recreate previous progress

--- script.js
+++ script.js
@@ -5,7 +5,14 @@
  * student_array - global array to hold student objects
  * @type {Array}
  */
-var student_array = [];
+var student_array = [
+    {studentName: 'first', course: 'frist', studentGrade: '0', deleted:false},
+    {studentName: 'second', course: 'secnod', studentGrade: '50', deleted:false},
+    {studentName: 'third', course: 'thrid', studentGrade: '100', deleted:false},
+    {studentName: 'four', course: 'fore', studentGrade: '25', deleted:false},
+    {studentName: 'fifth', course: 'fiff', studentGrade: '75', deleted:false}
+];
+
 /**
  * inputIds - id's of the elements that are used to add students
  * @type {string[]}
@@ -15,17 +22,15 @@ var inputIds = ['studentName','course','studentGrade'];
  * addClicked - Event Handler when user clicks the add button
  */
 function add_button(){
-    $('.add_student').click(function() {
-        addStudent();
-    });
+    addStudent();
+    updateData();
+    clearAddStudentForm();
 }
 /**
  * cancelClicked - Event Handler when user clicks the cancel button, should clear out student form
  */
 function cancelClicked() {
-    $('.cancel_student').click(function () {
-        clearAddStudentForm();
-    });
+    clearAddStudentForm();
 }
 /**
  * addStudent - creates a student objects based on input fields in the form and adds the object to global student array
@@ -34,7 +39,7 @@ function cancelClicked() {
 
 function addStudent() {
     console.log('addStudent function');
-    var student_obj = {};
+    var new_student  = {};
     for (var i = 0; i < inputIds.length; i++) { //looping through inputIds and using jquery to get value using html classes
         var student_index = inputIds[i];
         var value = $('#' + student_index).val();
@@ -42,11 +47,22 @@ function addStudent() {
         console.log('index: ', student_index);
         console.log('value: ', value);
     }
-    if(student_obj.studentName !== "" && student_obj.course !== "" && student_obj.studentGrade !== "") {
-        student_array.push(student_obj);
+
+    var matchNotFound = true;
+    for (student in student_array) {
+        //if already present, don't put into array
+        if (student_array[student].studentName == new_student.name &&
+            student_array[student].course == new_student.course &&
+            student_array[student].deleted == false) {
+            matchNotFound = false;
+            break;
+        }
     }
-    clearAddStudentForm();
-    updateData();
+    //if not present, add to array
+    if (matchNotFound) {
+        student_array.push(new_student);
+    }
+    return;
 }
 /**
   clearAddStudentForm - clears out the form values based on inputIds variable
@@ -60,9 +76,15 @@ function addStudent() {
 function clearAddStudentForm() {
     for (var i = 0; i < inputIds.length; i++) {
         var index = inputIds[i];
-        var value = $('#' + index).val("");
+        $('#' + index).val("");
     }
 }
+
+function removeStudent(studentObj)
+{
+    student_array[student_array.indexOf(studentObj)]
+}
+
 /**
  * calculateAverage - loop through the global student array and calculate average grade and return that value
  * @returns {number}
@@ -129,7 +151,5 @@ function reset() {
  * Listen for the document to load and reset the data to the initial state
  */
 $(document).ready(function () {
-    add_button(); //addClicked function call to add button click function
-    cancelClicked(); //cancelClicked function call to cancel button click function
     reset(); //reset function loaded to reset application to default state
 });
