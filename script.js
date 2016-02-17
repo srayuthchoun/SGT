99f46f5c0a25bfa3f4f1e77d4dff6dde3a22067c
srayuth choun <srayuthchoun@gmail.com>
2016-02-16 17:32:40 -0800

    working addStudentToDom function

--- script.js
+++ script.js
@@ -44,17 +44,17 @@ function cancelClicked() {
 function addStudent() {
     console.log('addStudent function');
     var student_obj = {};
-    for (var i = 0; i < inputIds.length; i++) { //looping through inputIds and using jquery to get value within the classes
+    for (var i = 0; i < inputIds.length; i++) { //looping through inputIds and using jquery to get value within the  html classes
         console.log('loop', i);
         var student_index = inputIds[i];
         var value = $('#' + student_index).val();
-        //var value = input.val();
         student_obj[student_index] = value;
         console.log('index: ', student_index);
         console.log('value: ', value);
     }
-    student_array.push(student_obj);
+        student_array.push(student_obj);
     clearAddStudentForm();
+    updateData();
 }
 
 /**
@@ -76,10 +76,10 @@ function clearAddStudentForm() {
 function calculateAverage() {
     var total = 0;
     var average= 0;
-    for (var i = 0; i < student_array.length; i++) {
+    for (var i = 0; i < student_array.length; i++) { //looping through student_array for studentGrade and add to total
         total += parseFloat(student_array[i].studentGrade);
     }
-    var average = Math.round(total / student_array.length);
+    var average = Math.round(total / student_array.length); //Calculation for the average
     console.log('average: ', average);
     return average;
 }
@@ -91,14 +91,24 @@ function calculateAverage() {
 function updateData() {
     var average = calculateAverage();
     $('.avgGrade').html(average);
-
+    updateStudentList();
 }
 
 /**
  * updateStudentList - loops through global student array and appends each objects data into the student-list-container > list-body
  */
 function updateStudentList() {
+    if (student_array.length == 0) {
+        var user_unavail_msg = $('<td>').attr("colspan", 6).append($('<h2>').html("User Info Unavailable"));
+    }
 
+    $('.student-list tbody').html('');
+
+    for(var i = 0; i < student_array.length; i++){
+        var student_object = student_array[i];
+        console.log('updateStudentList function, student object: ', student_object);
+        addStudentToDom(student_object);
+    }
 }
 
 /**
@@ -107,6 +117,15 @@ function updateStudentList() {
  * @param studentObj
  */
 
+function addStudentToDom(student_object) {
+    var new_table_row = $('<tr>');
+    var newCol1 = $('<td>').html(student_object.studentName);
+    var newCol2 = $('<td>').html(student_object.course);
+    var newCol3 = $('<td>').html(student_object.studentGrade);
+    new_table_row.append(newCol1, newCol2, newCol3);
+    $('.student-list > tbody').append(new_table_row);
+
+}
 /**
  * reset - resets the application to initial state. Global variables reset, DOM get reset to initial load state
  */
