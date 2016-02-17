5dd86b3abeff7897c09012bf386c5f33b6a4a0e3
Nicholas Bidler <nomandoestheimpossible@gmail.com>
2016-02-17 14:39:32 -0800

    SGT v.5 - Nick - trying to recreate previous progress pt2

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function addStudent() {
     for (var i = 0; i < inputIds.length; i++) { //looping through inputIds and using jquery to get value using html classes
         var student_index = inputIds[i];
         var value = $('#' + student_index).val();
-        student_obj[student_index] = value;
+        new_student[student_index] = value;
         console.log('index: ', student_index);
         console.log('value: ', value);
     }
@@ -53,7 +53,8 @@ function addStudent() {
         //if already present, don't put into array
         if (student_array[student].studentName == new_student.name &&
             student_array[student].course == new_student.course &&
-            student_array[student].deleted == false) {
+            student_array[student].deleted == false)
+        {
             matchNotFound = false;
             break;
         }
@@ -82,23 +83,44 @@ function clearAddStudentForm() {
 
 function removeStudent(studentObj)
 {
-    student_array[student_array.indexOf(studentObj)]
+    student_array[student_array.indexOf(studentObj)].deleted=true;
 }
 
 /**
  * calculateAverage - loop through the global student array and calculate average grade and return that value
  * @returns {number}
  */
+
 function calculateAverage() {
     var total = 0;
     var average= 0;
-    for (var i = 0; i < student_array.length; i++) { //looping through student_array for studentGrade and add to total
-        total += parseFloat(student_array[i].studentGrade);
+    var deletedEntries=0;
+    //if nothing in array, return 0
+    if (student_array.length > 0)
+    {
+        for (var i = 0; i < student_array.length; i++)
+        {
+            //if valid entry, add to total
+            if (student_array[i].deleted == false)
+            {
+                total += parseFloat(student_array[i].studentGrade);
+            }
+            //if not, skip and add to deleted entries count
+            else
+            {
+                deletedEntries++;
+            }
+        }
+        //if more than 1 valid entry, calculate average
+        if (student_array.length > deletedEntries)
+        {
+            average = (total / (student_array.length - deletedEntries));
+        }
     }
-    average = Math.round(total / student_array.length); //Calculation for the average
-    console.log('average: ', average);
+    //if array length <1 OR no un-deleted entries
     return average;
 }
+
 /**
  * updateData - centralized function to update the average and call student list update
  */
@@ -111,45 +133,91 @@ function updateData() {
  * updateStudentList - loops through global student array and appends each objects data into the student-list-container > list-body
  */
 function updateStudentList() {
+    var currentName;
+    var currentCourse;
 
-    $('.student-list > tbody').html('');
-
-    for(var i = 0; i < student_array.length; i++){
-        var student_object = student_array[i];
-        console.log('updateStudentList function, student object: ', student_object);
-        addStudentToDom(student_object);
-    }
-    if (student_array.length == 0) {
-        var user_unavail_msg = $('<td>').attr("colspan", 6).append($('<h2>').html("User Info Unavailable"));
-        $('.student-list tbody').html(user_unavail_msg);
+    for (student in student_array) {//loop through student_array
+        //if entry deleted, skip to next
+        if(student_array[student].deleted){
+            continue;
+        }
+        //take name and course
+        currentName = student_array[student].studentName;
+        currentCourse = student_array[student].course;
+        //assumes entry is not already displayed
+        var matchNotFound = true;
+        //finds number of rows are already displayed in DOM
+        var currentRows = $('tr').length;
+        //Except for table head, loop through displayed rows
+        for (var i = 0; i < currentRows; i++) {
+            //target row
+            var row = $('tr:nth-of-type(' + (i + 1) + ')');
+            //if name-course pair is already displayed, then doesn't need to be added to display
+            if (
+                (row.find('td:first-of-type').text() == currentName) &&
+                (row.find('td:nth-of-type(2)').text() == currentCourse))
+            {
+                //match found, don't loop further, skip to next entry
+                matchNotFound = false;
+                break;
+            }
+        }
+        //loops through and none of displayed names are new entry
+        if (matchNotFound) {
+            //add new entry to DOM
+            addStudentToDom(student_array[student]);
+        }
     }
 }
+
 /**
  * addStudentToDom - take in a student object, create html elements from the values and then append the elements
  * into the .student_list tbody
  * @param studentObj
  */
-function addStudentToDom(studentObj){
 
-    //TODO: don't do a loop here, pass in one object at a time via the parameter studentObj
-        var newStudentName = $("<td>").html(studentObj.studentName);
-        var newStudentCourse = $("<td>").html(studentObj.course);
-        var newStudentGrade = $("<td>").html(studentObj.studentGrade);
-        var newTableRow = $("<tr>");
-        newTableRow.append(newStudentName,newStudentCourse,newStudentGrade);
-        $(".student-list tbody").append(newTableRow);
+function addStudentToDom(studentObj)//meant to add one student to the DOM, one object in the array
+// is passed into this function
+{
+    var studentRow = $('<tr>');//studentRow is now a table row
+    //var studentNameTD = $('<td>').text(studentObj.name);
+    var studentNameTD = $('<td>',{
+        text: studentObj.studentName
+    });
+    var studentCourseTD = $('<td>',{
+        text: studentObj.course
+    });
+    var studentGradeTD = $('<td>',{
+        text: studentObj.studentGrade
+    });
+    var studentButtonTD = $('<td>');
+    var delete_button = $('<button>',{
+        type: 'button',
+        class: 'btn btn-danger',
+        text: 'Delete'
+    });
+    delete_button.click(function(){
+        removeStudent(studentObj);
+        $(this).parent().parent().remove();
+        updateData();
+    });
+    studentButtonTD.append(delete_button);
+    studentRow.append(studentNameTD, studentCourseTD, studentGradeTD, studentButtonTD);
+    $('tbody').append(studentRow);
 }
 /**
  * reset - resets the application to initial state. Global variables reset, DOM get reset to initial load state
  */
 function reset() {
     student_array = [];
-    clearAddStudentForm();
     updateData();
+    clearAddStudentForm();
 }
 /**
  * Listen for the document to load and reset the data to the initial state
  */
 $(document).ready(function () {
-    reset(); //reset function loaded to reset application to default state
+    updateData();
+    //reset(); //reset function loaded to reset application to default state
+    //comment out reset to load with dummy data
 });
